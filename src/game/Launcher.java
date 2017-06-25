3fa7db8dafcf3c67dc4f5a833244c3e39b50d47b
Marco Colombo <m.colombo@ed.ac.uk>
2017-06-25 11:18:28 +0100

    Use a variable for the window with in the Launcher class.

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -2,7 +2,8 @@ package game;
 
 public class Launcher {
     public static void main(String[] args) {
-        Game game = new Game("Poop Game", 800, 800 / 16 * 9);
+        final int width = 800;
+        Game game = new Game("Poop Game", width, width / 16 * 9);
         game.start();
     }
 }
