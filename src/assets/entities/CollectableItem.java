fa0aee8f3cf0c6aab6a9561ec865cc8565792bae
Marco Colombo <m.colombo@ed.ac.uk>
2017-07-05 08:51:01 +0100

    Make an item disappear after 10 seconds of being spawned.

--- src/assets/entities/CollectableItem.java
+++ src/assets/entities/CollectableItem.java
@@ -4,6 +4,9 @@ import assets.Assets;
 
 public class CollectableItem extends StaticEntity {
 
+    /** Interval for which the item is displayed in milliseconds. */
+    private static final long DISPLAY_INTERVAL = 10000;
+
     /** Type of item. */
     private final Items item;
 
@@ -15,6 +18,13 @@ public class CollectableItem extends StaticEntity {
         super(Assets.items[item.ordinal()], x, y, 64, 64);
         this.item = item;
         setBounds(18, 18, 32, 32);
+        spawnTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void tick() {
+        if (System.currentTimeMillis() - spawnTime > DISPLAY_INTERVAL)
+            collected = true;
     }
 
     /** Collects the item. */
